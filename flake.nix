1c1a0829bde4c807b39eaa0eccdb5b2570ec4c28
Alexandre Negrel <alexandre@negrel.dev>
2025-07-25 22:47:26 +0200

    update deps and toolchain

--- flake.nix
+++ flake.nix
@@ -1,25 +1,37 @@
 {
-  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
     let
       outputsWithoutSystem = { };
-      outputsWithSystem = flake-utils.lib.eachDefaultSystem (system:
-        let pkgs = import nixpkgs { inherit system; };
-        in {
+      outputsWithSystem = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
           devShells = {
-            default = pkgs.mkShell { buildInputs = with pkgs; [ go gopls ]; };
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                go
+                gopls
+              ];
+            };
           };
           packages = {
             default = pkgs.buildGoModule {
               name = "gengeommdb";
               version = "0.1.0";
-              vendorHash =
-                "sha256-WK5iIQKYnbIXLklsyflnoSQssV/aBi3RrYhX5bqa5lY=";
+              vendorHash = "sha256-YLFSLxKRIMWJSPc3i98HycsNqC+TycEFO9Ba3dLTMyU=";
 
               src = ./.;
             };
           };
-        });
-    in outputsWithSystem // outputsWithoutSystem;
+        }
+      );
+    in
+    outputsWithSystem // outputsWithoutSystem;
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,13 @@
 module github.com/negrel/gengeommdb
 
-go 1.20
+go 1.23.0
+
+toolchain go1.23.2
+
+require github.com/maxmind/mmdbwriter v1.0.0
 
 require (
-	github.com/maxmind/mmdbwriter v1.0.0 // indirect
-	github.com/oschwald/maxminddb-golang v1.12.0 // indirect
-	go4.org/netipx v0.0.0-20220812043211-3cc044ffd68d // indirect
-	golang.org/x/sys v0.10.0 // indirect
+	github.com/oschwald/maxminddb-golang v1.13.1 // indirect
+	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba // indirect
+	golang.org/x/sys v0.34.0 // indirect
 )
